42d6ff8a9dbaa6c3e60492fda62e05dc0870900c
Mandi97 <pawel.mandrysz@gmail.com>
2023-04-05 23:15:12 +0200

    add about me page

--- home/urls.py
+++ home/urls.py
@@ -6,4 +6,5 @@ app_name = 'home'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('o_mnie', views.AboutMe.as_view(), name='o-mnie'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class HomeView(TemplateView):
     template_name = 'home/home.html'
+
+
+class AboutMe(TemplateView):
+    template_name = 'home/about_me.html'
