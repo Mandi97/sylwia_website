f5d1c6a68f58b7f063545cf769b44710731e1245
Mandi97 <pawel.mandrysz@gmail.com>
2023-04-05 18:59:25 +0200

    fix settings and add image

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import environ
 import os
 
+from django.template.context_processors import media
+
 env = environ.Env(
     DEBUG=(bool, False)
 )
@@ -136,6 +138,9 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
